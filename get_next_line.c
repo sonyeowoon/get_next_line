36b4bfd63ac4262574eb6bca1d263f53dbe31419
Sanghee Seo <sangseo@c3r15s3.42gyeongsan.kr>
2024-05-25 17:59:42 +0900

    v9

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sangseo <sangseo@student.42gyeongsan.      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/22 04:06:14 by sangseo           #+#    #+#             */
-/*   Updated: 2024/05/25 04:46:43 by sangseo          ###   ########.fr       */
+/*   Updated: 2024/05/25 16:36:01 by sangseo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,36 @@ static char	*chk_nl(char **buf)
 	return (0);
 }
 
-char	*get_line(int fd, char **save, char *tmp)
+static ssize_t	chk_read(int fd, char *buf, char **tmp)
+{
+	ssize_t	read_l;
+
+	read_l = read(fd, buf, BUFFER_SIZE);
+	if (read_l < 0)
+	{
+		free(buf);
+		if (*tmp)
+			free(*tmp);
+		*tmp = 0;
+	}
+	else if (read_l == 0)
+		free(buf);
+	return (read_l);
+}
+
+static char	*nl_return(char *tmp, char *buf)
+{
+	if (tmp)
+	{
+		tmp = ft_allocfree(tmp, buf, 1);
+		free(buf);
+		return (tmp);
+	}
+	else
+		return (buf);
+}
+
+static char	*get_line(int fd, char **save, char *tmp)
 {
 	ssize_t	read_l;
 	char	*buf;
@@ -42,30 +71,13 @@ char	*get_line(int fd, char **save, char *tmp)
 	read_l = 1;
 	while (read_l > 0)
 	{
-		read_l = read(fd, buf, BUFFER_SIZE);
-		if (read_l < 0)
-		{
-			free(buf);
-			return (0);
-		}
-		if (read_l == 0)
-		{
-			free(buf);
+		read_l = chk_read(fd, buf, &tmp);
+		if (read_l <= 0)
 			return (tmp);
-		}
 		buf[read_l] = 0;
 		*save = chk_nl(&buf);
 		if (*save)
-		{
-			if (tmp)
-			{
-				tmp = ft_allocfree(tmp, buf, 1);
-				free(buf);
-				return (tmp);
-			}
-			else
-				return (buf);
-		}
+			return (nl_return(tmp, buf));
 		if (!tmp)
 			tmp = ft_strdup(buf);
 		else if (tmp)
